3d1e9b142251a41acfd128156178715ecd08f845
Brennan McMicking <brennanmcmicking@gmail.com>
2021-11-09 09:39:55 -0800

    Make timezone daylight-savings-aware Note: many code changes in this commit are caused by the "prettier" auto-formatter

--- schedule_downloader.py
+++ schedule_downloader.py
@@ -1,11 +1,12 @@
 import json
 import requests
 from datetime import date, datetime
-from icalendar import Calendar, Event, vRecur
+from icalendar import Calendar, Event, vRecur, vDatetime
 from datetime import datetime
 from getpass import getpass
 from bs4 import BeautifulSoup
 from os import remove
+from pytz import timezone
 
 """
 Your Netlink ID is never stored and is only sent to UVic's servers.
@@ -21,7 +22,7 @@ CACHE_PAGE = False
 
 def get_execution():
     url = "https://www.uvic.ca/cas/login"
-    
+
     response = requests.get(url)
     page = BeautifulSoup(response.text, 'html.parser')
 
@@ -35,19 +36,21 @@ def get_TGC(username, password, execution):
 
     data = f"username={username}&password={password}&execution={execution}&_eventId=submit"
     headers = {'Content-Type': "application/x-www-form-urlencoded"}
-    querystring = {"service":"https://www.uvic.ca/home/tools/index.php"}
+    querystring = {"service": "https://www.uvic.ca/home/tools/index.php"}
 
-    response = requests.request("POST", url, data=data, headers=headers, params=querystring, allow_redirects=False)
+    response = requests.request(
+        "POST", url, data=data, headers=headers, params=querystring, allow_redirects=False)
     return response.cookies.get("TGC")
-
 
 
 def get_SESSID(TGC):
     url = "https://www.uvic.ca/cas/login"
 
-    querystring = {"service":"https://www.uvic.ca/BAN1P/banuvic.gzcaslib.P_Service_Ticket?target=bwskflib.P_SelDefTerm"}
+    querystring = {
+        "service": "https://www.uvic.ca/BAN1P/banuvic.gzcaslib.P_Service_Ticket?target=bwskflib.P_SelDefTerm"}
     headers = {'Cookie': f"TGC={TGC}"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
 
     return response.cookies.get("SESSID")
 
@@ -60,7 +63,7 @@ def get_detailed_courses(SESSID, term):
         'Content-Type': "application/x-www-form-urlencoded",
         'Cookie': f"SESSID={SESSID}"
     }
-    response = requests.request("POST", url, data=data, headers=headers )
+    response = requests.request("POST", url, data=data, headers=headers)
 
     return(response)
 
@@ -77,8 +80,7 @@ def read_course_values(page):
 
     for t in page.select("table.datadisplaytable"):
         caption = t.select("caption.captiontext")[0].text
-        #print(caption)
-
+        # print(caption)
 
         if caption == "Scheduled Meeting Times":
             scheduleArray = []
@@ -90,11 +92,11 @@ def read_course_values(page):
                 # all of the values are in this array
                 fields = row.select("td.dddefault")
 
-                schedule["Type"]          = fields[0].text.strip()
-                schedule["Time"]          = fields[1].text.strip()
-                schedule["Days"]          = fields[2].text.strip()
-                schedule["Where"]         = fields[3].text.strip()
-                schedule["Date Range"]    = fields[4].text.strip()
+                schedule["Type"] = fields[0].text.strip()
+                schedule["Time"] = fields[1].text.strip()
+                schedule["Days"] = fields[2].text.strip()
+                schedule["Where"] = fields[3].text.strip()
+                schedule["Date Range"] = fields[4].text.strip()
                 schedule["Schedule Type"] = fields[5].text.strip()
                 # ignore instructors because we already have them
 
@@ -102,29 +104,30 @@ def read_course_values(page):
 
             # finally update the dictionary
             course_dict[current_course]["Schedule"] = scheduleArray
-            
+
         else:
             course_values = {}
             current_course = caption
-            
-            fields = t.select("td.dddefault")
-            course_values["Term"]        = fields[0].text.strip()
-            course_values["CRN"]         = fields[1].text.strip()
-            course_values["Status"]      = fields[2].text.strip()
-            course_values["Instructors"] = fields[3].text.strip().split(",")
-            course_values["Grade Mode"]  = fields[4].text.strip()
-            course_values["Credits"]     = fields[5].text.strip()
-            course_values["Level"]       = fields[6].text.strip()
-            course_values["Campus"]      = fields[7].text.strip()
 
-            # try removing empty profs			
+            fields = t.select("td.dddefault")
+            course_values["Term"] = fields[0].text.strip()
+            course_values["CRN"] = fields[1].text.strip()
+            course_values["Status"] = fields[2].text.strip()
+            course_values["Instructors"] = fields[3].text.strip().split(",")
+            course_values["Grade Mode"] = fields[4].text.strip()
+            course_values["Credits"] = fields[5].text.strip()
+            course_values["Level"] = fields[6].text.strip()
+            course_values["Campus"] = fields[7].text.strip()
+
+            # try removing empty profs
             try:
                 course_values["Instructors"].remove("")
             except ValueError:
                 pass
 
             for i in range(len(course_values["Instructors"])):
-                course_values["Instructors"][i] = course_values["Instructors"][i].strip() 
+                course_values["Instructors"][i] = course_values["Instructors"][i].strip(
+                )
 
             course_dict[current_course] = course_values
 
@@ -137,9 +140,9 @@ def parse_course_dict(course_dict):
     for course_name in course_dict:
         course = course_dict[course_name]
         parsed_course = {}
-        
-        parsed_course["title"]   = course_name.split(" - ")[0]
-        parsed_course["code"]    = course_name.split(" - ")[1]
+
+        parsed_course["title"] = course_name.split(" - ")[0]
+        parsed_course["code"] = course_name.split(" - ")[1]
         parsed_course["section"] = course_name.split(" - ")[2]
 
         parsed_course["schedule"] = []
@@ -151,13 +154,13 @@ def parse_course_dict(course_dict):
                 continue
 
             new_schedule["start_time"] = sched["Time"].split(" - ")[0]
-            new_schedule["end_time"]   = sched["Time"].split(" - ")[1]
+            new_schedule["end_time"] = sched["Time"].split(" - ")[1]
 
             new_schedule["start_date"] = sched["Date Range"].split(" - ")[0]
-            new_schedule["end_date"]   = sched["Date Range"].split(" - ")[1]
+            new_schedule["end_date"] = sched["Date Range"].split(" - ")[1]
 
             new_schedule["location"] = sched["Where"]
-            
+
             new_schedule["days"] = days_to_ics(sched["Days"])
 
             parsed_course["schedule"].append(new_schedule)
@@ -166,6 +169,7 @@ def parse_course_dict(course_dict):
         parsed_dict[course["CRN"]] = parsed_course
 
     return parsed_dict
+
 
 def days_to_ics(day_string):
     # This function lookS at the letters "MTWRFS" and converts them to weekdays
@@ -185,7 +189,8 @@ def days_to_ics(day_string):
             days.append("FR")
         elif c == "S":
             days.append("SA")
-    return days 
+    return days
+
 
 def ics_day_to_number(day):
     # all days, including sunday
@@ -207,12 +212,10 @@ def ics_day_to_number(day):
 
 
 def create_ics(courses, filename):
-    # UVic is GMT-7
-    tz = "-0700"
     # Unix "date" style format string for datetime importing
     # Example:
     #   "Jan 01, 2021 12:59 PM -0700"
-    tformat = "%b %d, %Y %I:%M %p %z"
+    tformat = "%b %d, %Y %I:%M %p"
 
     cal = Calendar()
     for c in courses:
@@ -223,9 +226,15 @@ def create_ics(courses, filename):
             event = Event()
 
             # Time formatting is similar to unix "date" command
-            start_time = datetime.strptime(f'{schedule["start_date"]} {schedule["start_time"]} {tz}', tformat)
-            end_time   = datetime.strptime(f'{schedule["start_date"]} {schedule["end_time"]  } {tz}', tformat)
-            end_date   = datetime.strptime(f'{schedule["end_date"]  } {schedule["end_time"]  } {tz}', tformat)
+            start_time = datetime.strptime(
+                f'{schedule["start_date"]} {schedule["start_time"]}', tformat)
+
+            end_time = datetime.strptime(
+                f'{schedule["start_date"]} {schedule["end_time"]}', tformat)
+
+            end_date = datetime.strptime(
+                f'{schedule["end_date"]  } {schedule["end_time"]}', tformat)
+
             days = schedule["days"]
 
             numeric_days = []
@@ -238,12 +247,15 @@ def create_ics(courses, filename):
                     start_time = start_time.replace(day=start_time.day+1)
                 except ValueError:
                     if start_time.month == 12:
-                        start_time = start_time.replace(day=1, month=1, year=start_time.year+1)
+                        start_time = start_time.replace(
+                            day=1, month=1, year=start_time.year+1)
                     else:
-                        start_time = start_time.replace(day=1, month=start_time.month+1)
+                        start_time = start_time.replace(
+                            day=1, month=start_time.month+1)
             else:
                 # update end time accordingly
-                end_time = datetime.combine(date=start_time.date(), time=end_time.time())
+                end_time = datetime.combine(
+                    date=start_time.date(), time=end_time.time())
 
             event.add("summary", f"{course['code']} {course['section']}")
 
@@ -259,12 +271,25 @@ def create_ics(courses, filename):
                 "until": end_date,
                 "byday": days
             }))
-            
+
             cal.add_component(event)
-    
+
     with open(f"{filename}.ics", "wb") as f:
         f.write(cal.to_ical())
-        f.close()
+
+    filedata = None
+    with open(f"{filename}.ics", "r") as f:
+        filedata = f.read()
+
+    # We must insert the TZID field into the DTSTART and DTEND fields. I don't know why this is the convention, but it is.
+    # America/Vancouver, US/Pacific, and America/Los_Angeles are all valid timezone names
+    outputdata = filedata.replace("DTSTART;VALUE=DATE-TIME",
+                                  "DTSTART;TZID=America/Vancouver")
+    outputdata = outputdata.replace("DTEND;VALUE=DATE-TIME",
+                                    "DTEND;TZID=America/Vancouver")
+
+    with open(f"{filename}.ics", "w") as f:
+        f.write(outputdata)
 
 
 def getTerm():
@@ -298,8 +323,6 @@ def getTerm():
         print("Invalid term. Options are (sp)ring, (su)mmer, (f)all")
         exit(1)
 
-    
-
 
 def fetch_page():
     try:
@@ -312,18 +335,18 @@ def fetch_page():
         exit(0)
 
     print("Downloading...")
-    
+
     execution = get_execution()
-    #print(execution)
+    # print(execution)
     TGC = get_TGC(username, password, execution)
-    
+
     if (TGC == None):
         print("Error: Failed to log in.")
         exit(1)
 
-    #print(TGC)
+    # print(TGC)
     SESSID = get_SESSID(TGC)
-    #print(SESSID)
+    # print(SESSID)
 
     detailed_courses = get_detailed_courses(SESSID, term)
 
@@ -333,7 +356,7 @@ def fetch_page():
 
     if CACHE_PAGE:
         open("page.html", "wb").write(detailed_courses.content)
-    
+
     return (page, term)
 
 
@@ -347,11 +370,11 @@ def main():
                 page = BeautifulSoup(in_file.read(), 'html.parser')
                 in_file.close()
         term = "testing"
-                
-    except FileNotFoundError:
-        page, term = fetch_page()	
 
-    print("Parsing...")        
+    except FileNotFoundError:
+        page, term = fetch_page()
+
+    print("Parsing...")
     course_dict_raw = read_course_values(page)
 
     if course_dict_raw == {}:
@@ -360,7 +383,7 @@ def main():
     # print(json.dumps(course_dict_raw, indent=4, sort_keys=False))
 
     parsed_courses = parse_course_dict(course_dict_raw)
- 
+
     # print(json.dumps(parsed_courses, indent=4, sort_keys=False))
 
     filename = "schedule"
